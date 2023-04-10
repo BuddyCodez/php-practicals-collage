1fd8f838a0326db9403346e81a7ddba6fa106a68
BuddyCodezgit config --global user.email riddhi990@outlook.comgit config --global user.name BuddyCodez <92288214+BuddyCodez@users.noreply.github.com>
2023-04-10 08:35:27 +0530

    practical 2 commit

---
+++ 2.1/input.php
@@ -0,0 +1,22 @@
+<h1>
+    Udit Vegad(216090907001)
+</h1>
+<form action="output.php" method="POST">
+    <input type="text" name="fname" placeholder="First Name">
+    <input type="text" name="lname" placeholder="Last name">
+    <br />
+    <input type="number" name="age" placeholder="Age">
+    <input type="email" name="email" placeholder="Email">
+    <br />
+    <input type="number" name="phone" placeholder="Phone">
+    <input type="text" name="address" placeholder="Address">
+    <br />
+    <input type="text" name="city" placeholder="City">
+    <input type="text" name="state" placeholder="State">
+    <br />
+    <input type="number" name="zip" placeholder="Zip">
+    <input type="text" name="country" placeholder="Country">
+    <br />
+
+    <input type="submit" value="Submit">
+</form>
---
+++ 2.1/output.php
@@ -0,0 +1,14 @@
+<?php
+echo "User Details: <br />";
+echo "First Name: " . $_POST["fname"] . "<br />";
+echo "Last Name: " . $_POST["lname"] . "<br />";
+echo "Age: " . $_POST["age"] . "<br />";
+echo "Email: " . $_POST["email"] . "<br />";
+echo "Phone: " . $_POST["phone"] . "<br />";
+echo "Address: " . $_POST["address"] . "<br />";
+echo "City: " . $_POST["city"] . "<br />";
+echo "State: " . $_POST["state"] . "<br />";
+echo "Zip: " . $_POST["zip"] . "<br />";
+echo "Country: " . $_POST["country"] . "<br />";
+
+?>
