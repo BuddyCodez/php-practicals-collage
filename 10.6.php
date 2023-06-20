67aff8098f7dde2416bacae054e4a48362a79544
BuddyCodezgit config --global user.email riddhi990@outlook.comgit config --global user.name BuddyCodez <92288214+BuddyCodez@users.noreply.github.com>
2023-06-20 21:03:12 +0530

    new update

--- 10.6.php
+++ 10.6.php
@@ -5,6 +5,12 @@ if (!isset($_SESSION['username'])) {
     header("Location:10.2.php");
 }
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $pass = $_POST['pass'];
+    $name = $_POST['name'];
+    $query = "UPDATE employees SET password='$pass' where name='$name'";
+    $con = mysqli_connect("localhost", "root", "", "test");
+    $result = mysqli_query($con, $query);
+    header("Location:10.4.php");
 }
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $name = $_SESSION['username'];
@@ -18,7 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 <!-- Display employee information -->
 <h1>Change Password</h1>
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-    <input type="text" name='name' value="<?php echo $name; ?>">
-    <input type="text" name='name' value="<?php echo $pass['password']; ?>">
+    <input type="text" name='name' value="<?php echo $name; ?>" readonly>
+    <input type="text" name='pass' value="<?php echo $pass['password']; ?>">
     <button type="submit">Submit</button>
 </form>
